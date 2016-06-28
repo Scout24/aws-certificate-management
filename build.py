ff1e1197221eef2856421e8ace7e98d3b4137efe
Felix Borchers <felix.borchers@immobilienscout24.de>
2016-06-28 11:34:10 +0200

    fixed teamcity, missing plugin

--- build.py
+++ build.py
@@ -5,6 +5,7 @@ use_plugin("python.unittest")
 use_plugin("python.install_dependencies")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
+use_plugin("python.distutils")
 use_plugin('copy_resources')
 use_plugin("filter_resources")
 
@@ -15,6 +16,7 @@ default_task = ["clean", "analyze", "publish"]
 @init
 def set_properties(project):
     pass
+
 
 @init(environments='teamcity')
 def set_properties_for_teamcity_builds(project):
