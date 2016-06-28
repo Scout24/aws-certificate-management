46b7b57db38f0e584c8fdec37d78b3b57de612d5
Felix Borchers <felix.borchers@immobilienscout24.de>
2016-06-28 11:09:06 +0200

    added teamcity config

--- build.py
+++ build.py
@@ -13,3 +13,18 @@ default_task = ["clean", "analyze", "publish"]
 @init
 def set_properties(project):
     pass
+
+@init(environments='teamcity')
+def set_properties_for_teamcity_builds(project):
+    import os
+    project.set_property('teamcity_output', True)
+    project.version = '%s.%s' % (project.version,
+                                 os.environ.get('BUILD_NUMBER', 0))
+    project.default_task = ['clean', 'install_build_dependencies', 'publish']
+    project.set_property('install_dependencies_index_url',
+                         os.environ.get('PYPIPROXY_URL'))
+    project.rpm_release = os.environ.get('RPM_RELEASE', 0)
+    project.set_property('copy_resources_target', '$dir_dist')
+    project.get_property('copy_resources_glob').extend(['setup.cfg'])
+    project.get_property('filter_resources_glob').extend(['**/setup.cfg'])
+
