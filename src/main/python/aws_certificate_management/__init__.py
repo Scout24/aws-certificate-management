528bc2843846b99268c8145162b68148434d5eef
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2016-07-05 17:29:42 +0200

    First draft of a CLI tool that uses the new Python backend

--- src/main/python/aws_certificate_management/__init__.py
+++ src/main/python/aws_certificate_management/__init__.py
@@ -1,1 +1,6 @@
 from __future__ import print_function, absolute_import, division
+
+from .ses import setup_ses_rule_set
+from .configure_dns import create_ses_dns_records
+
+__all__ = ["setup_ses_rule_set", "create_ses_dns_records"]
