dc93230b9da78d984975231d3c0e7a05a8bbfb41
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2016-07-11 17:23:20 +0200

    Move code from CLI script into the module
    This makes it accessible for integration tests.

--- src/main/python/aws_certificate_management/__init__.py
+++ src/main/python/aws_certificate_management/__init__.py
@@ -1,10 +1,25 @@
 from __future__ import print_function, absolute_import, division
 
-from .ses import setup_ses_rule_set
-from .ses import cleanup_ses_rule_set
+import os
+
+from .ses import setup_ses_rule_set, cleanup_ses_rule_set
 from .configure_dns import create_ses_dns_records
 from .configure_dns import delete_ses_dns_records_and_bucket
 
 
 __all__ = ["setup_ses_rule_set", "create_ses_dns_records",
-           "delete_ses_dns_records_and_bucket", "cleanup_ses_rule_set"]
+           "delete_ses_dns_records_and_bucket", "cleanup_ses_rule_set",
+           "setup_certificate", "cleanup"]
+
+
+def setup_certificate(domain, region):
+    mail_bucket_name = create_ses_dns_records(domain)
+    setup_ses_rule_set(domain, mail_bucket_name)
+
+    request_certificate = "aws acm request-certificate --domain-name '{domain}' --region='{region}'"
+    os.system(request_certificate.format(domain=domain, region=region))
+
+
+def cleanup(domain):
+    delete_ses_dns_records_and_bucket(domain)
+    cleanup_ses_rule_set(domain)
