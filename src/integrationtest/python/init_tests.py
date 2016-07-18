6013feaafcd01002d13a822f1d0e747ce70fffd5
Stefan Nordhausen <stefan.nordhausen@immobilienscout24.de>
2016-07-18 17:27:12 +0200

    Also test if www domains can be set up twice

--- src/integrationtest/python/init_tests.py
+++ src/integrationtest/python/init_tests.py
@@ -3,6 +3,7 @@ import unittest2
 from aws_certificate_management import cleanup, setup_certificate
 
 WILDCARD_DOMAIN = '*.pro-test.wolke.is'
+WWW_DOMAIN = 'www.pro-test.wolke.is'
 ACM_ARN_PREFIX = 'arn:aws:acm:eu-west-1:'
 ACM_ARN_PREFIX2 = 'arn:aws:acm:eu-central-1:'
 
@@ -19,10 +20,18 @@ class SetupCertificateTests(unittest2.TestCase):
             else:
                 return
 
-    def test_setup_certificate_called_twice(self):
+    def test_setup_certificate_called_twice_wildcard(self):
         with self.assertLogs(logger='aws-certificate-management', level='INFO') as cm:
             setup_certificate(WILDCARD_DOMAIN, 'eu-west-1')
             setup_certificate(WILDCARD_DOMAIN, 'eu-west-1')
+
+        logged_messages = "".join(cm.output)
+        self.assertIn(ACM_ARN_PREFIX, logged_messages)
+
+    def test_setup_certificate_called_twice_www(self):
+        with self.assertLogs(logger='aws-certificate-management', level='INFO') as cm:
+            setup_certificate(WWW_DOMAIN, 'eu-west-1')
+            setup_certificate(WWW_DOMAIN, 'eu-west-1')
 
         logged_messages = "".join(cm.output)
         self.assertIn(ACM_ARN_PREFIX, logged_messages)
